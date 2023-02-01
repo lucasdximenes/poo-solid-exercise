e1d8b8632e465766e7dc711580cf7d446d4cab95
Lucas Ximenes <lucasdximenes@gmail.com>
2023-02-01 10:33:43 -0300

    feat: :rocket: implement Monster class

---
+++ src/Monster.ts
@@ -0,0 +1,32 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this.strength);
+  }
+
+  receiveDamage(attackPoints: number): number {
+    if (attackPoints > this.lifePoints) {
+      this._lifePoints = -1;
+      return this.lifePoints;
+    }
+    this._lifePoints -= attackPoints;
+    return this.lifePoints;
+  }
+}
